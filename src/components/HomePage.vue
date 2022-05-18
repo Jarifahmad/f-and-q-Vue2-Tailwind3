a49b81423eacab3e5e22a7981e4935b6fce529ac
Jarif Ahmad <jarifahmad@yahoo.com>
2022-05-18 16:46:05 +0100

    new

---
+++ src/components/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+    
+    <div class="shadow-2xl box-border h-20 w-20 p-4 border-4 border-teal-400 bg-teal-200">
+        <h1>F&Q Component - Jarif</h1>
+
+        <h2 class="bg-rose-500"> test </h2>
+    </div>
+    
+
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/input/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
