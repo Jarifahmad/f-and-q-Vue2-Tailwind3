3947bc5a87ed19150da6c7c45cf4b079afbd8128
Jarif Ahmad <jarifahmad@yahoo.com>
2022-05-21 21:40:40 +0100

    Changed numbered variables such as (1,2,3) to words (one, two...)

--- src/components/FandQ.vue
+++ src/components/FandQ.vue
@@ -4,14 +4,14 @@
         <h2 class="font-bold m-6 underline text-xl text-indigo-700">Frequently asked question </h2>
 
         <div class="flex flex-col items-center box-border h-full w-full p-4 border-4 border-blue-400 bg-blue-300">
-            <button @click.prevent="answerToQuestion1 = ! answerToQuestion1" class="rounded-full bg-white w-40 m-3">Question 1</button> 
-            <p v-if="answerToQuestion1" class="flex flex-cols-1 m-2"> Answer to question 1 is blah blah </p>
+            <button @click.prevent="answerToQuestionOne = ! answerToQuestionOne" class="rounded-full bg-white w-40 m-3">Question 1</button> 
+            <p v-if="answerToQuestionOne" class="flex flex-cols-1 m-2"> Answer to question 1 is blah blah </p>
 
-            <button @click.prevent="answerToQuestion2 = ! answerToQuestion2" class="rounded-full bg-white w-40 m-3">Question 2</button> 
-            <p v-if="answerToQuestion2" class="flex flex-cols-1 m-2"> Answer to question 2 is blah blah </p>
+            <button @click.prevent="answerToQuestionTwo = ! answerToQuestionTwo" class="rounded-full bg-white w-40 m-3">Question 2</button> 
+            <p v-if="answerToQuestionTwo" class="flex flex-cols-1 m-2"> Answer to question 2 is blah blah </p>
             
-            <button @click.prevent="answerToQuestion3 = !answerToQuestion3" class="rounded-full bg-white w-40 m-3">Question 3</button> 
-            <p v-if="answerToQuestion3" class="flex flex-cols-1 m-2"> Answer to question 3 is blah blah </p>
+            <button @click.prevent="answerToQuestionThree = !answerToQuestionThree" class="rounded-full bg-white w-40 m-3">Question 3</button> 
+            <p v-if="answerToQuestionThree" class="flex flex-cols-1 m-2"> Answer to question 3 is blah blah </p>
 
         </div>
 
@@ -25,9 +25,9 @@
 export default {
   data (){
       return{
-          answerToQuestion1: false,
-          answerToQuestion2: false,
-          answerToQuestion3: false,
+          answerToQuestionOne: false,
+          answerToQuestionTwo: false,
+          answerToQuestionThree: false,
       }
   },
 
