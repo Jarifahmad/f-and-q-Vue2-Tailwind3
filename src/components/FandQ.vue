4b3e7a5a93bc99e81fc2218f2a13e81e8f60286b
Jarif Ahmad <jarifahmad@yahoo.com>
2022-05-22 17:22:58 +0100

    got rid of w-40

--- src/components/FandQ.vue
+++ src/components/FandQ.vue
@@ -4,13 +4,13 @@
         <h2 class="font-bold m-6 underline text-xl text-indigo-700">Frequently asked question </h2>
 
         <div class="flex flex-col items-center box-border h-full w-full p-4 border-4 border-blue-400 bg-blue-300">
-            <button @click.prevent="answerToQuestionOne = ! answerToQuestionOne" class="rounded-full bg-white w-40 m-3">Question 1</button> 
+            <button @click.prevent="answerToQuestionOne = ! answerToQuestionOne" class="rounded-full bg-white m-3 p-2">Question 1</button> 
             <p v-if="answerToQuestionOne" class="flex flex-cols-1 m-2"> Answer to question 1 is blah blah </p>
 
-            <button @click.prevent="answerToQuestionTwo = ! answerToQuestionTwo" class="rounded-full bg-white w-40 m-3">Question 2</button> 
+            <button @click.prevent="answerToQuestionTwo = ! answerToQuestionTwo" class="rounded-full bg-white m-3 p-2">Question 2</button> 
             <p v-if="answerToQuestionTwo" class="flex flex-cols-1 m-2"> Answer to question 2 is blah blah </p>
             
-            <button @click.prevent="answerToQuestionThree = !answerToQuestionThree" class="rounded-full bg-white w-40 m-3">Question 3</button> 
+            <button @click.prevent="answerToQuestionThree = !answerToQuestionThree" class="rounded-full bg-white m-3 p-2">Question 3</button> 
             <p v-if="answerToQuestionThree" class="flex flex-cols-1 m-2"> Answer to question 3 is blah blah </p>
 
         </div>
