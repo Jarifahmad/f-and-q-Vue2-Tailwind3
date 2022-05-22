59fc61df4fbe9f769a7135b9dded19dae06e228e
Jarif Ahmad <jarifahmad@yahoo.com>
2022-05-22 18:39:22 +0100

    attached v-model to input fields

--- src/components/FormSection.vue
+++ src/components/FormSection.vue
@@ -5,18 +5,18 @@
             <form class="max-w-xl w-full text-gray-200 mt-4 px-8 md:px-0" action="#" method="post">
                 <div class="flex flex-col md:flex-row md:-mx-2">
                     <label for="first_name" class="sr-only">First Name</label>
-                    <input class="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="text" placeholder="First name" id="first_name">
+                    <input v-model="firstName" class="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="text" placeholder="First name" id="first_name">
                             
                     <label for="last_name" class="sr-only">Last Name</label>
-                    <input class="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="text" placeholder="Last name" id="last_name">
+                    <input v-model="lastName" class="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="text" placeholder="Last name" id="last_name">
                 </div>
 
                 <div class="flex flex-col md:flex-row  md:-mx-2">
                     <label for="email" class="sr-only">Email address</label>
-                    <input class="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="email" placeholder="john@email.com" id="email">
+                    <input v-model="email" class="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="email" placeholder="john@email.com" id="email">
                             
                     <label for="title" class="sr-only">Title</label>
-                    <input class="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="text" placeholder="Your title" id="title">
+                    <input v-model="title" class="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="text" placeholder="Your title" id="title">
                 </div>
                 <div class="flex flex-col md:flex-row md:-mx-2">
                     <button class="bg-blue-600 text-sm mt-4 rounded-lg py-2 px-4 w-full md:mx-2">Create account</button>
@@ -24,3 +24,19 @@
             </form>
     </div>
 </template>
+
+<script>
+
+export default {
+  data (){
+      return{
+          firstName: null,
+          lastName: null,
+          email: null,
+          title: null,
+      }
+  },
+
+}
+
+</script>
