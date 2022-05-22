f09d3abbeff51a83b67455220d9a255c68b12fbb
Jarif Ahmad <jarifahmad@yahoo.com>
2022-05-22 20:51:43 +0100

    changed from no caps to capital letter "EnrollButton"

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import enrollButton from './components/enrollButton.vue'
+import EnrollButton from './components/EnrollButton.vue'
 
 Vue.config.productionTip = false
 
-Vue.component(`enrollButton`, enrollButton)
+Vue.component(`EnrollButton`, EnrollButton)
 
 new Vue({
   render: h => h(App),
