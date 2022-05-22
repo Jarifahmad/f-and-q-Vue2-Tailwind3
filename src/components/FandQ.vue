26fb86d501af367a0a97715c8bbf89c0d0e5803e
Jarif Ahmad <jarifahmad@yahoo.com>
2022-05-22 20:06:21 +0100

    refactored "v-if" on F&Q section to "v-show"

--- src/components/FandQ.vue
+++ src/components/FandQ.vue
@@ -5,13 +5,13 @@
 
         <div class="flex flex-col items-center box-border h-full w-full p-4 border-4 border-blue-400 bg-blue-300">
             <button @click.prevent="answerToQuestionOne = ! answerToQuestionOne" class="rounded-full bg-white m-3 p-2">Question 1</button> 
-            <p v-if="answerToQuestionOne" class="flex flex-cols-1 m-2"> Answer to question 1 is blah blah </p>
+            <p v-show="answerToQuestionOne" class="flex flex-cols-1 m-2"> Answer to question 1 is blah blah </p>
 
             <button @click.prevent="answerToQuestionTwo = ! answerToQuestionTwo" class="rounded-full bg-white m-3 p-2">Question 2</button> 
-            <p v-if="answerToQuestionTwo" class="flex flex-cols-1 m-2"> Answer to question 2 is blah blah </p>
+            <p v-show="answerToQuestionTwo" class="flex flex-cols-1 m-2"> Answer to question 2 is blah blah </p>
             
             <button @click.prevent="answerToQuestionThree = !answerToQuestionThree" class="rounded-full bg-white m-3 p-2">Question 3</button> 
-            <p v-if="answerToQuestionThree" class="flex flex-cols-1 m-2"> Answer to question 3 is blah blah </p>
+            <p v-show="answerToQuestionThree" class="flex flex-cols-1 m-2"> Answer to question 3 is blah blah </p>
 
         </div>
 
