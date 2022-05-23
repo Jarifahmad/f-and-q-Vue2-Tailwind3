eec312b463f15a949204b2a33d0335cd4ae6e176
Jarif Ahmad <jarifahmad@yahoo.com>
2022-05-23 20:42:31 +0100

    included v-bind with props

--- src/components/PropButton.vue
+++ src/components/PropButton.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col justify-center items-center">
-        <button class="bg-blue-400 flex flex-col w-40 justify-center items-center mt-10 rounded-full border-black border-2">{{btnText}}</button>
+        <button class="bg-blue-400 flex flex-col w-40 p-2 justify-center items-center mt-10 rounded-full border-black border-2">{{btnText}}</button>
     </div>
 </template>
 
