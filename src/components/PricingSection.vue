8f5c4a212b03c9128cbc651cde9f347a2bb9edd1
Jarif Ahmad <jarifahmad@yahoo.com>
2022-05-26 17:21:01 +0100

    Included Pricing section

---
+++ src/components/PricingSection.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex justify-center mt-12">
+        <div class="flex flex-col min-w-0 min-h-0 bg-neutral-600 items-center p-6">
+            <h1 class="flex justify-center text-white text-4xl font-bold m-6 ">Free Forever</h1>
+
+            <div class="flex w-14 h-3 bg-amber-300 justify-center"></div>
+            
+            <p class="text-white flex justify-center m-10 text-lg">For churches exploring the RaiseUp <br> Platformm with no cost. </p>
+
+            <p class="text-white flex justify-center text-lg">WHAT'S INCLUDED:</p>
+
+            <p class="text-white flex justify-center text-base font-bold mt-6">Unlimited trial period</p>
+
+            <div class="flex w-full h-0.5 bg-cyan-300 justify-center m-6 "></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
