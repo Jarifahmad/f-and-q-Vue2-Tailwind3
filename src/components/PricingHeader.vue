d14a196a35a3f77ede096ef2890a81b186f073f1
Jarif Ahmad <jarifahmad@yahoo.com>
2022-06-05 20:12:15 +0100

    updated pricing section

---
+++ src/components/PricingHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+<!--max-w-6xl border-b-8 border-amber-300  -->
+        <div class="flex justify-center flex-col ">
+            <slot name="pricing-header">
+                <h1 class="font-bold text-5xl text-black mb-6 mt-2 flex justify-center border-b-8 border-amber-300 ">A premium platform with mission-led pricing</h1> 
+            </slot>
+
+            <slot name="pricing-description">
+                <p class=" max-w-3xl justify-items-center text-left text-lg m-auto text-black mt-10 mb-12">
+                Our platform has accessible pricing plans for smaller and larger churches,
+                as well as a Free Forever plan that won't cost a thing. So whether your church 
+                has 5 children or 500, you can access the resources you need to raise up faith in the next generation.
+                </p>
+            </slot>
+        </div>
+
+    </div>
+</template>
+
+
+
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
--- src/components/PricingSection.vue
+++ src/components/PricingSection.vue
@@ -1,51 +1,127 @@
 <template>
-    <div class="flex justify-center mt-12">
+    <div>
+        <div class="flex flex-row justify-center">
+        <slot name="free-forever">
 
-        <!--(left side of the pricing table) Free Forever-->
+        <table class="max-w-md flex">
+            <ul class="justify-center  bg-neutral-600 p-8 text-center">
+                <li>
+                    <h1 class="flex justify-center text-white text-4xl font-bold m-6 ">Free Forever</h1>
+                </li>
+                
+                <li class="bg-amber-300 w-14 h-3"></li>
 
-        <div class="flex flex-col min-w-0 min-h-0 bg-neutral-600 items-center p-10">
-            <h1 class="flex justify-center text-white text-4xl font-bold m-6">Free Forever</h1>
+                <li>
+                    <p class="text-white m-10 text-lg text-center">For churches exploring the RaiseUp <br/> Platformm with no cost. </p>
+                </li>
 
-            <div class="flex w-14 h-3 bg-amber-300 justify-center"></div>
-            
-            <p class="text-white flex justify-center m-10 text-lg text-center">For churches exploring the RaiseUp <br/> Platformm with no cost. </p>
+                <li>
+                    <p class="text-white text-lg text-center">WHAT'S INCLUDED:</p>
+                </li>
 
-            <p class="text-white flex justify-center text-lg">WHAT'S INCLUDED:</p>
+                <li>
+                    <p class=" justify-items-center text-white text-base font-bold border-b-2 p-4 border-cyan-300">Unlimited trial period</p> 
+                </li>
 
-            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Unlimited trial period</p>
+                <li>
+                    <p class=" justify-items-center text-white text-base font-bold border-b-2 p-4 border-cyan-300">Access to a selection of resources</p>
+                </li>
 
-            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Access to a selection of resources</p>
+                <li>
+                   <p class="text-white text-base font-bold border-b-2 p-4 border-cyan-300 text-center">Streaming & downloads of <br/> selected resources</p> 
+                </li>
 
-            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300 text-center">Streaming & downloads of selected resources</p>
+                <li>
+                    <p class=" justify-items-center text-white text-base font-bold border-b-2 p-4 border-cyan-300">Featured videos</p>
+                </li>
 
-            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Featured videos</p>
-            
-            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 mb-10 border-cyan-300">No payment details required</p>
+                <li>
+                    <p class=" justify-items-center text-white text-base font-bold border-b-2 text-center p-4 border-cyan-300">No payment details required</p>
+                </li>
+            </ul> 
+        </table>
 
+        </slot>
+
+        <slot name="subscriber">
+        <table class="max-w-md ">
+            <ul class="bg-teal-600 p-8 justify-items-center text-center">
+                <li>
+                    <h1 class="flex justify-center text-white text-4xl font-bold m-6 ">Subscriber</h1>
+                </li>
+                <li>
+                    <div class="flex justify-center w-14 h-3 bg-amber-300"></div>
+                </li>
+
+                <li>
+                   <p class="text-white my-10 text-center text-lg ">For churches who would like full access, <br> with plans starting at $25 / £17 per month. </p>
+                </li>
+
+                <li>
+                    <p class="text-white text-lg">WHAT'S INCLUDED:</p>
+                </li>
+
+                <li>
+                    <p class=" justify-items-center text-white text-base font-bold border-b-2 p-4 border-cyan-300">Access to unlimited resources</p> 
+                </li>
+
+                <li>
+                    <p class=" justify-items-center text-white text-base font-bold border-b-2 p-4 border-cyan-300">Choose from 800+ lessons</p>
+                </li>
+
+                <li>
+                   <p class="text-white text-base font-bold border-b-2 p-4 border-cyan-300 text-center">Unlimited streaming & downloads <br>of all content</p> 
+                </li>
+
+                <li>
+                    <p class=" justify-items-center text-white text-base font-bold border-b-2 p-4 border-cyan-300">Lesson editor and builder</p>
+                </li>
+
+                <li>
+                    <p class=" justify-items-center text-white text-base font-bold border-b-2 p-4 mb-2 border-cyan-300">Unlimited licence for use in your <br>church</p>
+                </li>
+            </ul>
+        </table>
+
+        </slot>
         </div>
 
-        <!--(right side of the pricing table) Free Forever-->
+        
 
-        <div class="flex flex-col min-w-0 min-h-0 bg-teal-600 items-center p-10">
-            <h1 class="flex justify-center text-white text-4xl font-bold m-6">Subscriber</h1>
-
-            <div class="flex w-14 h-3 bg-amber-300 justify-center"></div>
-            
-            <p class="text-white flex justify-center m-10 text-lg text-center">For churches who would like full access <br/> with plans starting at $25 / £17 per month. </p>
-
-            <p class="text-white flex justify-center text-lg">WHAT'S INCLUDED:</p>
-
-            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Access to unlimited resources</p>
-
-            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Access to a selection of resources</p>
-
-            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300 text-center">Streaming & downloads of selected resources</p>
-
-            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Featured videos</p>
-            
-            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 mb-10 border-cyan-300">No payment details required</p>
-
-        </div>
+        <!--
+                        <table class="flex justify-center">
+                <ul class="flex  justify-items-center bg-teal-600 ">
+                    <li>
+                        <h1 class="text-white text-4xl font-bold m-6">Subscriber</h1>
+                    </li>
+                    <li>
+                        <div class="w-14 h-3 bg-amber-300 justify-center"></div>
+                    </li>
+                    <li>
+                        <p class="text-white m-10 text-lg text-center">For churches who would like full access, <br/> with plans starting at $25 / £17 per month. </p>
+                    </li>
+                    <li>
+                        <p class="text-white text-lg">WHAT'S INCLUDED:</p>
+                    </li>
+                    <li>
+                         <p class="text-white text-base font-bold border-b-2 w-80 p-4 border-cyan-300 text-center">Access to unlimited resources</p>
+                    </li>
+                    <li>
+                        <p class="text-white text-base font-bold border-b-2 w-80 p-4 border-cyan-300 text-center">Choose from 800+ lessons</p>
+                    </li>
+                    <li>
+                        <p class="text-white text-base font-bold border-b-2 w-80 p-4 border-cyan-300 text-center">Unlimited streaming & downloads of all content</p>
+                    </li>
+                    <li>
+                        <p class="text-white text-base font-bold border-b-2 w-80 p-4 border-cyan-300 text-center">Lesson editor and builder</p>
+                    </li>
+                    <li>
+                        <p class="text-white text-base font-bold border-b-2 w-80 p-4 mb-10 border-cyan-300 text-center">Unlimited licence for use in your church</p>
+                    </li>
+                </ul>
+        </table>
+        -->
+  
     </div>
 </template>
 
@@ -56,5 +132,4 @@
 </script>
 
 <style>
-
 </style>
