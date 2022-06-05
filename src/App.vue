8f6553f3cfaf2d518dcfb5e4cdce6cb5a0f60d0e
Jarif Ahmad <jarifahmad@yahoo.com>
2022-06-05 20:12:02 +0100

    updated

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,9 @@
       </template>
     </captioned-content> 
 
-    <captioned-content> 
-      <template #content>
-        <div class="flex max-w-xl h-80">
-          <img class="mt-4" src="https://w.wallhaven.cc/full/dp/wallhaven-dpo38l.jpg" alt="">
-        </div>
-      </template>
-      <template #caption>
-        Source: <a class="text-blue-900 underline" href="https://w.wallhaven.cc/full/dp/wallhaven-dpo38l.jpg">wallhaven</a>
-      </template>
-    </captioned-content> 
+    <PricingHeader/> 
 
-    
+    <PricingSection/>
 
     <HeroSection/>
 
@@ -51,6 +42,7 @@ import FormSection from './components/FormSection.vue'
 import InputField from './components/InputField.vue'
 import CaptionedContent from './components/CaptionedContent.vue'
 import NavBar from './components/NavBar.vue'
+import PricingSection from './components/PricingSection.vue'
 
 
 
@@ -65,6 +57,7 @@ export default {
     FooterSection,
     InputField,
     CaptionedContent,
+    PricingSection
 },
 
 }
--- src/components/PricingSection.vue
+++ src/components/PricingSection.vue
@@ -1,17 +1,50 @@
 <template>
     <div class="flex justify-center mt-12">
-        <div class="flex flex-col min-w-0 min-h-0 bg-neutral-600 items-center p-6">
-            <h1 class="flex justify-center text-white text-4xl font-bold m-6 ">Free Forever</h1>
+
+        <!--(left side of the pricing table) Free Forever-->
+
+        <div class="flex flex-col min-w-0 min-h-0 bg-neutral-600 items-center p-10">
+            <h1 class="flex justify-center text-white text-4xl font-bold m-6">Free Forever</h1>
 
             <div class="flex w-14 h-3 bg-amber-300 justify-center"></div>
             
-            <p class="text-white flex justify-center m-10 text-lg">For churches exploring the RaiseUp <br> Platformm with no cost. </p>
+            <p class="text-white flex justify-center m-10 text-lg text-center">For churches exploring the RaiseUp <br/> Platformm with no cost. </p>
 
             <p class="text-white flex justify-center text-lg">WHAT'S INCLUDED:</p>
 
-            <p class="text-white flex justify-center text-base font-bold mt-6">Unlimited trial period</p>
+            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Unlimited trial period</p>
 
-            <div class="flex w-full h-0.5 bg-cyan-300 justify-center m-6 "></div>
+            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Access to a selection of resources</p>
+
+            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300 text-center">Streaming & downloads of selected resources</p>
+
+            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Featured videos</p>
+            
+            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 mb-10 border-cyan-300">No payment details required</p>
+
+        </div>
+
+        <!--(right side of the pricing table) Free Forever-->
+
+        <div class="flex flex-col min-w-0 min-h-0 bg-teal-600 items-center p-10">
+            <h1 class="flex justify-center text-white text-4xl font-bold m-6">Subscriber</h1>
+
+            <div class="flex w-14 h-3 bg-amber-300 justify-center"></div>
+            
+            <p class="text-white flex justify-center m-10 text-lg text-center">For churches who would like full access <br/> with plans starting at $25 / £17 per month. </p>
+
+            <p class="text-white flex justify-center text-lg">WHAT'S INCLUDED:</p>
+
+            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Access to unlimited resources</p>
+
+            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Access to a selection of resources</p>
+
+            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300 text-center">Streaming & downloads of selected resources</p>
+
+            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 border-cyan-300">Featured videos</p>
+            
+            <p class="text-white flex justify-center text-base font-bold border-b-2 w-80 p-4 mb-10 border-cyan-300">No payment details required</p>
+
         </div>
     </div>
 </template>
